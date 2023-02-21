e823dc5c4acd5b69fbab4e5664c5af5d2f9c1fd7
williammunyakathuo <tjwellieyam@gmail.com>
2023-02-21 09:43:24 +0300

    additional two side bars

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Leftnav from './components/Leftnav';
+import Rightnav from './components/Rightnav';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Leftnav/>
+     
+     
+      <Rightnav/>
+      
+    </div>
+  );
+}
+
+export default App;
