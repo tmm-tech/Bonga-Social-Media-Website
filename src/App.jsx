cf7fdeeeb92fd57e7276d20b4e0fb9ad43a9f653
williammunyakathuo <tjwellieyam@gmail.com>
2023-02-21 10:57:28 +0300

    sidebars changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css';
+import Leftnav from './Components/Leftnav';
+import Rightnav from './Components/Rightnav';
+
+
 
 function App() {
   return (
     <div className="App">
+     
+      <Leftnav/>
+     
+     
+      <Rightnav/>
       
     </div>
   );
