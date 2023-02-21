c25f889f6f8a33809dacdc226a4261b89757d7f4
tonym <tonymugi074@gmail.com>
2023-02-21 10:23:57 +0300

    Login page andpost display

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import './App.css';
-import InputPost from './Components/InputPost';
-import LoginForm from './Pages/LoginForm';
-import PostDisplay from './Components/PostDisplay'
+
 function App() {
   return (
     <div className="App">
-       <LoginForm/>
-      <InputPost/> 
-    <PostDisplay/> 
+
     </div>
   );
 }
