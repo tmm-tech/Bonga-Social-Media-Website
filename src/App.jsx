f7f89563364c9ab7f6c890ceb9faf46d3c3964d8
tonym <tonymugi074@gmail.com>
2023-02-21 09:42:49 +0300

    Login page/Post Display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
 import './App.css';
-
+import InputPost from './Components/InputPost';
+import LoginForm from './Pages/LoginForm';
+import PostDisplay from './Components/PostDisplay'
 function App() {
   return (
     <div className="App">
-      
+       <LoginForm/>
+      <InputPost/> 
+    <PostDisplay/> 
     </div>
   );
 }
---
+++ src/Components/PostDisplay.jsx
@@ -0,0 +1,186 @@
+import React from "react";
+import image8 from '../Assets/admin2.jpg';
+import post10 from '../Assets/post/10.jpeg'
+ function PostDisplay() {
+  return (
+    <div className='Display'>
+            <div ClassName="loadMore">
+								<div ClassName="central-meta item">
+									<div ClassName="user-post">
+										<div ClassName="friend-info">
+											<figure>
+												<img src={image8} alt=""/>
+											</figure>
+											<div ClassName="friend-name">
+												<ins><a href="#" title="">Janice Griffith</a></ins>
+												<span>published: june,2 2018 19:PM</span>
+											</div>
+											<div ClassName="post-meta">
+												<img src={post10} alt=""/>
+												<div ClassName="we-video-info">
+													<ul>
+														<li>
+															<span ClassName="views" data-toggle="tooltip" title="views">
+																<i ClassName="fa fa-eye"></i>
+																<ins>1.2k</ins>
+															</span>
+														</li>
+														<li>
+															<span ClassName="comment" data-toggle="tooltip" title="Comments">
+																<i ClassName="fa fa-comments-o"></i>
+																<ins>52</ins>
+															</span>
+														</li>
+														<li>
+															<span ClassName="like" data-toggle="tooltip" title="like">
+																<i ClassName="ti-heart"></i>
+																<ins>2.2k</ins>
+															</span>
+														</li>
+														<li>
+															<span ClassName="dislike" data-toggle="tooltip" title="dislike">
+																<i ClassName="ti-heart-broken"></i>
+																<ins>200</ins>
+															</span>
+														</li>
+														<li ClassName="social-media">
+															<div ClassName="menu">
+															  <div ClassName="btn trigger"><i ClassName="fa fa-share-alt"></i></div>
+															  <div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-html5"></i></a></div>
+															  </div>
+															  <div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-facebook"></i></a></div>
+															  </div>
+															  <div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-google-plus"></i></a></div>
+															  </div>
+															  <div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-twitter"></i></a></div>
+															  </div>
+															  <div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-css3"></i></a></div>
+															  </div>
+															  <div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-instagram"></i></a>
+																</div>
+															  </div>
+																<div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-dribbble"></i></a>
+																</div>
+															  </div>
+															  <div ClassName="rotater">
+																<div ClassName="btn btn-icon"><a href="#" title=""><i ClassName="fa fa-pinterest"></i></a>
+																</div>
+															  </div>
+
+															</div>
+														</li>
+													</ul>
+												</div>
+												<div ClassName="description">
+													
+													<p>
+														World's most beautiful car in Curabitur <a href="#" title="">#test drive booking !</a> the most beatuiful car available in america and the saudia arabia, you can book your test drive by our official website
+													</p>
+												</div>
+											</div>
+										</div>
+										<div ClassName="coment-area">
+											<ul ClassName="we-comet">
+												<li>
+													<div ClassName="comet-avatar">
+														<img src="images/resources/comet-1.jpg" alt=""/>
+													</div>
+													<div ClassName="we-comment">
+														<div ClassName="coment-head">
+															<h5><a href="time-line.html" title="">Jason borne</a></h5>
+															<span>1 year ago</span>
+															<a ClassName="we-reply" href="#" title="Reply"><i ClassName="fa fa-reply"></i></a>
+														</div>
+														<p>we are working for the dance and sing songs. this car is very awesome for the youngster. please vote this car and like our post</p>
+													</div>
+													<ul>
+														<li>
+															<div ClassName="comet-avatar">
+																<img src="images/resources/comet-2.jpg" alt=""/>
+															</div>
+															<div ClassName="we-comment">
+																<div ClassName="coment-head">
+																	<h5><a href="time-line.html" title="">alexendra dadrio</a></h5>
+																	<span>1 month ago</span>
+																	<a ClassName="we-reply" href="#" title="Reply"><i ClassName="fa fa-reply"></i></a>
+																</div>
+																<p>yes, really very awesome car i see the features of this car in the official website of <a href="#" title="">#Mercedes-Benz</a> and really impressed :-)</p>
+															</div>
+														</li>
+														<li>
+															<div ClassName="comet-avatar">
+																<img src="images/resources/comet-3.jpg" alt=""/>
+															</div>
+															<div ClassName="we-comment">
+																<div ClassName="coment-head">
+																	<h5><a href="time-line.html" title="">Olivia</a></h5>
+																	<span>16 days ago</span>
+																	<a ClassName="we-reply" href="#" title="Reply"><i ClassName="fa fa-reply"></i></a>
+																</div>
+																<p>i like lexus cars, lexus cars are most beautiful with the awesome features, but this car is really outstanding than lexus</p>
+															</div>
+														</li>
+													</ul>
+												</li>
+												<li>
+													<div ClassName="comet-avatar">
+														<img src="images/resources/comet-1.jpg" alt=""/>
+													</div>
+													<div ClassName="we-comment">
+														<div ClassName="coment-head">
+															<h5><a href="time-line.html" title="">Donald Trump</a></h5>
+															<span>1 week ago</span>
+															<a ClassName="we-reply" href="#" title="Reply"><i ClassName="fa fa-reply"></i></a>
+														</div>
+														<p>we are working for the dance and sing songs. this video is very awesome for the youngster. please vote this video and like our channel
+															<i ClassName="em em-smiley"></i>
+														</p>
+													</div>
+												</li>
+												<li>
+													<a href="#" title="" ClassName="showmore underline">more comments</a>
+												</li>
+												<li ClassName="post-comment">
+													<div ClassName="comet-avatar">
+														<img src="images/resources/comet-1.jpg" alt=""/>
+													</div>
+													<div ClassName="post-comt-box">
+														<form method="post">
+															<textarea placeholder="Post your comment"></textarea>
+															<div ClassName="add-smiles">
+																<span ClassName="em em-expressionless" title="add icon"></span>
+															</div>
+															<div ClassName="smiles-bunch">
+																<i ClassName="em em---1"></i>
+																<i ClassName="em em-smiley"></i>
+																<i ClassName="em em-anguished"></i>
+																<i ClassName="em em-laughing"></i>
+																<i ClassName="em em-angry"></i>
+																<i ClassName="em em-astonished"></i>
+																<i ClassName="em em-blush"></i>
+																<i ClassName="em em-disappointed"></i>
+																<i ClassName="em em-worried"></i>
+																<i ClassName="em em-kissing_heart"></i>
+																<i ClassName="em em-rage"></i>
+																<i ClassName="em em-stuck_out_tongue"></i>
+															</div>
+															<button type="submit"></button>
+														</form>	
+													</div>
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+              </div>
+							</div>	
+  );
+}
+export default PostDisplay;
